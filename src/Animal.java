2c8d2b2fe7ae26bcf4d9666b3acde47cecbdd9da
Jellywhs <haosen@udel.edu>
2019-09-16 17:24:21 -0400

    haosen

---
+++ src/Animal.java
@@ -0,0 +1,17 @@
+
+public class Animal {
+	int Legs;
+	String name;
+	
+	public Animal(int Legs, String name) {
+		this.Legs = Legs;
+		this.name = name;
+	}
+	public int getLegs() {
+		return Legs;
+	}
+	public String getname() {
+		return name;
+	}
+
+}
